427a743573fb642b255b722a072324a9b6d455a5
Baldrani <mael.mayon@free.fr>
2018-04-09 22:45:49 +0200

    Add Test Gulp

--- cakephp/gulpfile.js
+++ cakephp/gulpfile.js
@@ -1,35 +1,89 @@
-const elixir = require('cakephp3-elixir');
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var babel = require('gulp-babel');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var rename = require('gulp-rename');
+var copy = require('gulp')
+var cleanCSS = require('gulp-clean-css');
+var del = require('del');
+
+var paths = {
+  styles: {
+    src: 'assets/sass/*.sass',
+    dest: 'webroot/css/'
+  },
+  scripts: {
+    src: 'assets/js/*.js',
+    dest: 'webroot/js/'
+  },
+  assets: {
+    src: ['**','!/**'],
+    dest: 'webroot/'
+  }
+};
+
+/* Not all tasks need to use streams, a gulpfile is just another node program
+ * and you can use all packages available on npm, but it must return either a
+ * Promise, a Stream or take a callback and call it
+ */
+function clean() {
+  // You can use multiple globbing patterns as you would with `gulp.src`,
+  // for example if you are using del 2.0 or above, return its promise
+  return del([ 'webroot' ]);
+}
 
 /*
- |--------------------------------------------------------------------------
- | Elixir Extensions
- |--------------------------------------------------------------------------
- |
- | Elixir provides several extensions which bring new functionality to Elixir.
- | A couple of examples have been included below to get you started.
- | You can use any extension made for the most recent version of Elixir 6.
- | Feel free to try out different extensions!
- | Please report any problems to: https://github.com/pfuri/cakephp-elixir/issues
- |
+ * Define our tasks using plain functions
  */
-//require('laravel-elixir-del');
-//require('laravel-elixir-webpack-official');
+function styles() {
+  return gulp.src(paths.styles.src)
+    .pipe(sass())
+    .pipe(cleanCSS())
+    // pass in options to the stream
+    .pipe(rename({
+      basename: 'main',
+      suffix: '.min'
+    }))
+    .pipe(gulp.dest(paths.styles.dest));
+}
+
+function scripts() {
+  return gulp.src(paths.scripts.src, { sourcemaps: true })
+    .pipe(babel())
+    .pipe(uglify())
+    .pipe(concat('main.min.js'))
+    .pipe(gulp.dest(paths.scripts.dest));
+}
+
+function watch() {
+  gulp.watch(paths.scripts.src, scripts);
+  gulp.watch(paths.styles.src, styles);
+}
+
+function copyFile(){
+    return gulp.src(paths.assets.src)
+        .pipe(gulp.dest(paths.assets.dest));
+}
+/*
+ * You can use CommonJS `exports` module notation to declare tasks
+ */
+exports.clean = clean;
+exports.styles = styles;
+exports.scripts = scripts;
+exports.watch = watch;
 
 /*
- |--------------------------------------------------------------------------
- | Elixir Asset Management
- |--------------------------------------------------------------------------
- |
- | Elixir provides a clean, fluent API for defining some basic Gulp tasks
- | for your Laravel application. By default, we are compiling the Sass
- | file for our application, as well as publishing vendor resources.
- |
+ * Specify if tasks run in series or parallel using `gulp.series` and `gulp.parallel`
  */
-elixir(mix => {
-        // Example
-		//mix.del(["webroot/build", "webroot/css", "webroot/js"])
-    	//.sass('default.scss').version('css/default.css')
-        //.webpack('default.js').version('js/default.js');
+var build = gulp.series(clean, gulp.parallel(styles, scripts), copyFile);
 
-        mix.sass('default.scss');
-});
+/*
+ * You can still use `gulp.task` to expose tasks
+ */
+gulp.task('build', build);
+
+/*
+ * Define default task that can be called by just running `gulp` from cli
+ */
+gulp.task('default', build);
