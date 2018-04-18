e3a7abf80fed12358eb0040c7646ba4af0aeadc1
Baldrani <mael.mayon@free.fr>
2018-04-19 01:21:51 +0200

    Test Import jQuery outside the box

--- cakephp/assets/Restaurant/js/index.js
+++ cakephp/assets/Restaurant/js/index.js
@@ -1,11 +1,5 @@
-//TODO find a way to pass over export manualy done
-import jQuery from 'jquery'
-window.$ = window.jQuery = jQuery;
+import './modules/bootstrap.js'
 
-import 'bootstrap'
 
-import toastr from 'toastr'
-window.toastr = toastr
-
-//import './modules/navbar.js'
-//import './modules/contact.js'
+import './modules/navbar.js'
+import './modules/contact.js'
